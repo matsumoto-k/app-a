dd0fe197c58010d61f7b17fa7a2173f1991a0334
matsumoto-k <ww.kei10@gmail.com>
2019-10-05 16:57:56 +0900

    ああああああ

--- App3/App3/App3/Views/MainPage.xaml.cs
+++ App3/App3/App3/Views/MainPage.xaml.cs
@@ -2,18 +2,20 @@
 using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
 
 namespace App3.Views
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : TabbedPage
+    public partial class MainPage : Xamarin.Forms.TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
 
+            On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
         }
     }
 }
