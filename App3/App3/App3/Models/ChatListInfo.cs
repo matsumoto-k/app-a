beece0bb2da8bab503219eee141336fbe3c6d821
matsumoto-k <ww.kei10@gmail.com>
2019-10-05 23:57:56 +0900

    2019年10月5日　最終

---
+++ App3/App3/App3/Models/ChatListInfo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3.Models
+{
+    class ChatListInfo
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ App3/App3/App3/Views/ChatListPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using App3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using System.Collections.ObjectModel;
+
+namespace App3.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChatListPage : ContentPage
+    {
+        public ChatListPage()
+        {
+            InitializeComponent();
+
+         
+        }
+    }
+}
